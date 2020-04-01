3e7c220d70809f457d5047bd6b1e65f1a621c66c
Rasha ali <rashaali84@gmail.com>
2020-04-01 20:15:06 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Content_mangement_portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
